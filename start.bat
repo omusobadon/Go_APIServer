9a9797909730b0ee88877aabc7212891f40bbab8
yaffu-kitune <22cc0203@jec.ac.jp>
2023-12-15 16:04:28 +0900

    update

--- start.bat
+++ start.bat
@@ -11,6 +11,13 @@ echo pgAdmin is createing
 podman compose -f .\docker-compose.yml up -d service_pgadmin
 
 echo pgAdmin is wait for starting...
+timeout /t 10
+
+echo Go_APIServer Setuping...
+go get github.com/steebchen/prisma-client-go
+go run github.com/steebchen/prisma-client-go db push
+
+echo seting...
 timeout /t 3
 
 echo service_app is createing..
