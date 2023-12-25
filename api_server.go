4e5c7ef5cd0c76ecc4b8407f698f84dce0b1bed9
22cc0207 <22cc0207@jec.ac.jp>
2023-12-25 10:55:29 +0900

    12-25-01

--- api_server.go
+++ api_server.go
@@ -17,6 +17,23 @@ const (
 	auto_insert bool = true
 )
 
+func CORSMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type,X-CSRF-Header,Authorization,Content-Type")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE")
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
 func APIServer() error {
 
 	// サーバ定義
@@ -42,15 +59,16 @@ func APIServer() error {
 
 	// 各ハンドラの呼び出し
 	// GET
-	http.HandleFunc("/get_shop", handlers.GetShop)
-	http.HandleFunc("/get_group", handlers.GetGroup)
-	http.HandleFunc("/get_product", handlers.GetProduct)
-	http.HandleFunc("/get_price", handlers.GetPrice)
-	http.HandleFunc("/get_seat", handlers.GetSeat)
-	http.HandleFunc("/get_stock", handlers.GetStock)
+	http.HandleFunc("/get_shop", CORSMiddleware(handlers.GetShop))
+	http.HandleFunc("/get_group", CORSMiddleware(handlers.GetGroup))
+	http.HandleFunc("/get_product", CORSMiddleware(handlers.GetProduct))
+	http.HandleFunc("/get_price", CORSMiddleware(handlers.GetPrice))
+	http.HandleFunc("/get_seat", CORSMiddleware(handlers.GetSeat))
+	http.HandleFunc("/get_stock", CORSMiddleware(handlers.GetStock))
+	http.HandleFunc("/get_manage", CORSMiddleware(handlers.GetManage))
 
 	// POST
-	http.HandleFunc("/post_order", handlers.PostOrder)
+	http.HandleFunc("/post_order", CORSMiddleware(handlers.PostOrder))
 
 	// http.HandleFunc("/order_change", handlers.OrderChange)
 	// http.HandleFunc("/manage_get", handlers.ManageGet)
