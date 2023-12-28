d552cdebed2e5397f589e09aa8a016ed6efefabf
22cc0207 <22cc0207@jec.ac.jp>
2023-12-28 17:44:18 +0900

    11-28-04

--- README.md
+++ README.md
@@ -39,21 +39,27 @@
 
 ### 車の予約注文をPOSTする際のJSONパラメータ
 - customer_id   : Customer（顧客情報）テーブルID
-- stock_id      : Stock（在庫情報）テーブルID
-- qty           : 数量
 - start_at      : 予約開始時刻
 - end_at        : 予約終了時刻
 - remark        : 備考
+- detail        : 注文詳細情報
+- stock_id      : Stock（在庫情報）テーブルID
+- qty           : 数量
+
 
 - json記述例
 ```json
 {
     "customer_id": 1,
-    "stock_id": 1,
-    "qty": 5,
-    "start_at": "2023-11-10T10:10:00+09:00",
-    "end_at": "2023-11-10T18:10:00+09:00",
-    "remark": "test"
+    "start_at": "2023-12-30T18:10:00+09:00",
+    "end_at": "2023-12-30T18:10:00+09:00",
+    "remark": "test",
+    "detail": [
+        {
+        "stock_id": 1,
+        "qty": 1
+        }
+    ]
 }
 ```
 
@@ -72,30 +78,36 @@
     "message": "正常終了",
     "request": {
         "customer_id": 1,
-        "stock_id": 1,
-        "seat_id": 0,
-        "qty": 1,
         "start_at": "2023-12-30T18:10:00+09:00",
         "end_at": "2023-12-30T18:10:00+09:00",
         "number_people": 0,
-        "remark": "test"
+        "remark": "test",
+        "detail": [
+            {
+                "stock_id": 1,
+                "seat_id": 0,
+                "qty": 1
+            }
+        ]
     },
     "order": {
-        "id": 13, // 注文番号
+        "id": 15,
         "customer_id": 1,
         "start_at": "2023-12-30T09:10:00Z",
         "end_at": "2023-12-30T09:10:00Z",
         "number_people": 0,
-        "is_accepted": true, // 注文が承認されたか
-        "created_at": "2023-12-28T06:59:10.697Z", // 注文日時
+        "is_accepted": true,
+        "created_at": "2023-12-28T08:40:28.848Z",
         "remark": "test"
     },
-    "order_detail": {
-        "id": 12, // 注文詳細番号
-        "order_id": 13, // 注文番号
-        "stock_id": 1,
-        "qty": 1
-    }
+    "order_detail": [
+        {
+            "id": 14,
+            "order_id": 15,
+            "stock_id": 1,
+            "qty": 1
+        }
+    ]
 }
 ```
 
--- handlers/post_order.go
+++ handlers/post_order.go
@@ -26,22 +26,26 @@ const (
 
 // リクエストを変換する構造体
 type PostOrderRequest struct {
-	Customer int       `json:"customer_id"`
-	Stock    int       `json:"stock_id"`
-	Seat     []int     `json:"seat_id"`
-	Qty      int       `json:"qty"`
-	Start    time.Time `json:"start_at"`
-	End      time.Time `json:"end_at"`
-	People   int       `json:"number_people"`
-	Remark   string    `json:"remark"`
+	Customer int                      `json:"customer_id"`
+	Start    time.Time                `json:"start_at"`
+	End      time.Time                `json:"end_at"`
+	People   int                      `json:"number_people"`
+	Remark   string                   `json:"remark"`
+	Detail   []PostOrderRequestDetail `json:"detail"`
+}
+
+type PostOrderRequestDetail struct {
+	Stock int `json:"stock_id"`
+	Seat  int `json:"seat_id"`
+	Qty   int `json:"qty"`
 }
 
 // レスポンスに変換する構造体（処理成功）
 type PostOrderResponseSuccess struct {
-	Message string              `json:"message"`
-	Request PostOrderRequest    `json:"request"`
-	Order   db.OrderModel       `json:"order"`
-	Detail  db.OrderDetailModel `json:"order_detail"`
+	Message string                `json:"message"`
+	Request PostOrderRequest      `json:"request"`
+	Order   db.OrderModel         `json:"order"`
+	Detail  []db.OrderDetailModel `json:"order_detail"`
 }
 
 // レスポンスに変換する構造体（処理失敗）
@@ -54,12 +58,12 @@ var post_order_cnt int // PostOrderのカウント用
 
 func PostOrder(w http.ResponseWriter, r *http.Request) {
 	post_order_cnt++
-	order := new(db.OrderModel)
-	detail := new(db.OrderDetailModel)
 	var (
 		status  int    = http.StatusNotImplemented
 		message string = "メッセージがありません"
 		req     PostOrderRequest
+		order   db.OrderModel
+		detail  []db.OrderDetailModel
 	)
 
 	fmt.Printf("*** Post Order No.%d ***\n", post_order_cnt)
@@ -78,8 +82,8 @@ func PostOrder(w http.ResponseWriter, r *http.Request) {
 			// レスポンスボディの作成
 			res.Message = message
 			res.Request = req
-			res.Order = *order
-			res.Detail = *detail
+			res.Order = order
+			res.Detail = detail
 
 			// レスポンス構造体をJSONに変換して送信
 			if err := json.NewEncoder(w).Encode(res); err != nil {
@@ -146,26 +150,6 @@ func PostOrder(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// 注文情報の在庫IDと一致する情報を取得
-	stock, err := client.Stock.FindUnique(
-		db.Stock.ID.Equals(req.Stock)).With(
-		db.Stock.Price.Fetch().With(
-			db.Price.Product.Fetch().With(
-				db.Product.Group.Fetch(),
-			),
-		),
-	).Exec(ctx)
-	if errors.Is(err, db.ErrNotFound) {
-		status = http.StatusBadRequest
-		message = "在庫情報がありません"
-		return
-	}
-	if err != nil {
-		status = http.StatusBadRequest
-		message = fmt.Sprint("在庫テーブル取得エラー : ", err)
-		return
-	}
-
 	// 現在時刻の取得
 	now := time.Now()
 
@@ -183,57 +167,82 @@ func PostOrder(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Productテーブルの予約開始可能期間(start_before)から予約開始可能時刻を計算
-	start_dur := time.Duration(stock.RelationsStock.Price.RelationsPrice.Product.RelationsProduct.Group.StartBefore)
-	start := now.Add(start_dur * time.Hour)
+	// 複数の注文の処理
+	for _, v := range req.Detail {
 
-	// 予約開始時刻が予約開始可能時刻より前のとき
-	if req.Start.Before(start) {
-		status = http.StatusBadRequest
-		message = "予約開始時刻までが短すぎます"
-		return
-	}
+		// 注文情報の在庫IDと一致する情報を取得
+		stock, err := client.Stock.FindUnique(
+			db.Stock.ID.Equals(v.Stock)).With(
+			db.Stock.Price.Fetch().With(
+				db.Price.Product.Fetch().With(
+					db.Product.Group.Fetch(),
+				),
+			),
+		).Exec(ctx)
+		if errors.Is(err, db.ErrNotFound) {
+			status = http.StatusBadRequest
+			message = "在庫情報がありません"
+			return
+		}
+		if err != nil {
+			status = http.StatusBadRequest
+			message = fmt.Sprint("在庫テーブル取得エラー : ", err)
+			return
+		}
 
-	// 予約可能期間の終了時刻を計算
-	end_dur := time.Duration(stock.RelationsStock.Price.RelationsPrice.Product.RelationsProduct.Group.AvailableDuration)
-	end := start.Add(end_dur * time.Hour)
+		// 在庫状態が有効かチェック
+		if !stock.IsEnable {
+			status = http.StatusBadRequest
+			message = fmt.Sprint("無効な在庫 : ", err)
+			return
+		}
 
-	// 予約開始時刻が予約可能期間より後のとき
-	if req.Start.After(end) {
-		status = http.StatusBadRequest
-		message = "まだ予約できません"
-		return
-	}
+		// Productテーブルの予約開始可能期間(start_before)から予約開始可能時刻を計算
+		start_dur := time.Duration(stock.RelationsStock.Price.RelationsPrice.Product.RelationsProduct.Group.StartBefore)
+		start := now.Add(start_dur * time.Hour)
 
-	// 在庫状態が有効かチェック
-	if !stock.IsEnable {
-		status = http.StatusBadRequest
-		message = fmt.Sprint("無効な在庫 : ", err)
-		return
-	}
+		// 予約開始時刻が予約開始可能時刻より前のとき
+		if req.Start.Before(start) {
+			status = http.StatusBadRequest
+			message = "予約開始時刻までが短すぎます"
+			return
+		}
 
-	// // 在庫が注文数よりも少ない場合はエラー
-	var qty int
-	if qty, _ = stock.Qty(); qty < req.Qty {
-		status = http.StatusBadRequest
-		message = "在庫不足"
-		return
-	}
+		// 予約可能期間の終了時刻を計算
+		end_dur := time.Duration(stock.RelationsStock.Price.RelationsPrice.Product.RelationsProduct.Group.AvailableDuration)
+		end := start.Add(end_dur * time.Hour)
 
-	// 在庫テーブルに注文情報を反映
-	_, err = client.Stock.FindUnique(
-		db.Stock.ID.Equals(req.Stock),
-	).Update(
-		db.Stock.Qty.Set(qty - req.Qty),
-	).Exec(ctx)
-	if err != nil {
-		status = http.StatusBadRequest
-		message = fmt.Sprint("在庫テーブルアップデートエラー : ", err)
-		return
+		// 予約開始時刻が予約可能期間より後のとき
+		if req.Start.After(end) {
+			status = http.StatusBadRequest
+			message = "まだ予約できません"
+			return
+		}
+
+		// // 在庫が注文数よりも少ない場合はエラー
+		var qty int
+		if qty, _ = stock.Qty(); qty < v.Qty {
+			status = http.StatusBadRequest
+			message = "在庫不足"
+			return
+		}
+
+		// 在庫テーブルに注文情報を反映
+		_, err = client.Stock.FindUnique(
+			db.Stock.ID.Equals(v.Stock),
+		).Update(
+			db.Stock.Qty.Set(qty - v.Qty),
+		).Exec(ctx)
+		if err != nil {
+			status = http.StatusBadRequest
+			message = fmt.Sprint("在庫テーブルアップデートエラー : ", err)
+			return
+		}
+
 	}
 
 	// Orderテーブルに注文情報をインサート
-	order, err = client.Order.CreateOne(
+	ord, err := client.Order.CreateOne(
 		db.Order.IsAccepted.Set(true),
 		db.Order.Customer.Link(
 			db.Customer.ID.Equals(req.Customer),
@@ -249,20 +258,26 @@ func PostOrder(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	order = *ord
+
 	// OrderDetailテーブルに注文情報をインサート
-	detail, err = client.OrderDetail.CreateOne(
-		db.OrderDetail.Order.Link(
-			db.Order.ID.Equals(order.ID),
-		),
-		db.OrderDetail.Stock.Link(
-			db.Stock.ID.Equals(req.Stock),
-		),
-		db.OrderDetail.Qty.Set(req.Qty),
-	).Exec(ctx)
-	if err != nil {
-		status = http.StatusBadRequest
-		message = fmt.Sprint("注文詳細テーブルインサートエラー : ", err)
-		return
+	for _, v := range req.Detail {
+		d, err := client.OrderDetail.CreateOne(
+			db.OrderDetail.Order.Link(
+				db.Order.ID.Equals(order.ID),
+			),
+			db.OrderDetail.Stock.Link(
+				db.Stock.ID.Equals(v.Stock),
+			),
+			db.OrderDetail.Qty.Set(v.Qty),
+		).Exec(ctx)
+		if err != nil {
+			status = http.StatusBadRequest
+			message = fmt.Sprint("注文詳細テーブルインサートエラー : ", err)
+			return
+		}
+
+		detail = append(detail, *d)
 	}
 
 	fmt.Println(order)
